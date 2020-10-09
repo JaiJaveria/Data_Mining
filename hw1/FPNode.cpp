33c23cff472fc99dd2936da775a81a3838a5503f
JaiJaveria <jaijaveria16@gmail.com>
2020-10-09 23:43:50 +0530

    key added in node

--- hw1/FPNode.cpp
+++ hw1/FPNode.cpp
@@ -5,15 +5,15 @@ class FPNode
 // private:
 // 	FPNode parent
 public:
-	FPNode()
-	// FPNode(int k)
+	// FPNode()
+	FPNode(int k)
 	{
 		parent=NULL;
 		counter=0;
-		// key=k;
+		key=k;
 	}
 	FPNode* parent;
-	// int key;
+	int key;
 	int counter;
 	unordered_map<int,FPNode*> children;
 	
