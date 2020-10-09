5cf4845b02d6e65c4b52a92df616fe16c02ab152
JaiJaveria <jaijaveria16@gmail.com>
2020-10-09 17:45:46 +0530

    Baskets sorted

--- hw1/FPInsert.cpp
+++ hw1/FPInsert.cpp
@@ -17,50 +17,93 @@ int main(int argc, char const *argv[])
 {
 	/* Reading the dat file */
 	ifstream datb;
+	int numT;
+	unordered_map<int, int> freqSin;//frequent singletons
 	//first argument is the path of database and second is the threshold percentage.
-	int x=(int) argv[2];
+	int x=atoi(argv[2]);
+	// cout << argv[1]<<"\n";
+	// cout << argv[2] << "\n";
+
 	datb.open(argv[1]);
 	if(!datb)
 	{
 		cout << "Not found";
 		return -1;
 	}
-	else
+	int data;
+	string d;
+	vector<pair<int,unsigned int>> freq;//to store the counters.
+	pair<int, unsigned int> p(0,0);
+	freq.push_back(p);
+	int item=1;
+	numT=0;
+	while(getline(datb, d))
 	{
-		int data;
-		string d;
-		vector<pair<int,unsigned int>> freq;//to store the counters.
-		pair<int, unsigned int> p(0,0);
-		freq.push_back(p);
-		int item=1;
-		int numT=0;
-		while(getline(datb, d))
+		numT++;
+		istringstream ss(d);
+		while( ss>> data)
 		{
-			numT++;
-			istringstream ss(d);
-			while( ss>> data)
+			if(data >=item)
 			{
-				if(data >=item)
+				while(item <= data)
 				{
-					while(item <= data)
-					{
-						pair<int, unsigned int> p(item,0);
-						freq.push_back(p);
-						item++;
-					}
+					pair<int, unsigned int> p(item,0);
+					freq.push_back(p);
+					item++;
 				}
-				freq[data].second++;
+			}
+			freq[data].second++;
+		}
+	}
+	float s=x*numT/100;
+	// sort(freq.begin(), freq.end(), compare);
+	// // now remove the infrequent ones.
+	// while(freq.back().second<s)
+	// {
+	// 	freq.pop_back();
+	// }
+	// for testing. outputting the frequent item list
+	// for (int i = 0; i < freq.size(); ++i)
+	// 	cout << freq[i].first << " : " << freq[i].second << "\n";
+
+	//make hash table of freqent ones
+	for (int i = 0; i < freq.size(); ++i)
+	{
+		
+		if(freq[i].second>=s)
+		{
+			freqSin[freq[i].first]=freq[i].second;
+		}
+	}
+	
+	freq.clear();
+	ifstream datbA;
+	datbA.open(argv[1]);
+	if(!datbA)
+	{
+		cout << "Not found";
+		return -1;
+	}
+	std::vector<pair<int,int>> basket;
+	while(getline(datbA, d))
+	{
+		basket.clear();
+		istringstream ss(d);
+		while( ss>> data)
+		{
+			if(freqSin.find(data)!=freqSin.end())
+			{
+				pair<int,int> p(data,freqSin[data]);
+				basket.push_back(p);
 			}
 		}
-		sort(freq.begin(), freq.end(), compare);
+		sort(basket.begin(), basket.end(), compare);
+		// for (int i = 0; i < basket.size(); ++i)
+		// {
+		// 	cout << basket[i].first << " ";
+		// }
+		// cout << "\n";
 		
-		auto s=x*numT/100;
-		while(freq.back().second<s)
-		{
-			freq.pop_back();
-		}
-		// for (int i = 0; i < freq.size(); ++i)
-		// 	cout << freq[i].first << " : " << freq[i].second << "\n";
 	}
 	return 0;
 }
