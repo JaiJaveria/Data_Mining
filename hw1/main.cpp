6d7ae2d4f999fc9ca3ab0a14765f35e5330d0e49
Pratik Prawar <pratikprawar@gmail.com>
2020-10-11 22:58:50 +0530

    some changes

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -6,27 +6,25 @@ using namespace std;
 
 
 int main(){
-    FPNode a = FPNode(1);
+    FPNode *a = new FPNode(1);
     vector<int> v = {1,3,9,8,8};
     vector<int> v2 = {1,2,5,4};
-    a.insert(v,1,0);
-    a.insert(v2,1,0);
-    FPNode p = a;
+    a->insert(v,1,0);
+    a->insert(v2,1,0);
+    FPNode *p = a;
     for(int i=0;i<5;i++){
-    if(a.children.find(v[i])!=a.children.end()){
+    if(a->children.find(v[i])!=a->children.end()){
        
-        FPNode* b = (a.children[v[i]]); 
-        a=*b;
-        cout<<a.key<<" "<<a.counter<<endl;
+        a = (a->children[v[i]]); 
+        cout<<a->key<<" "<<a->counter<<endl;
         
     }
     }
     for(int i=0;i<4;i++){
-    if(p.children.find(v2[i])!=p.children.end()){
-       
-        FPNode *b = (p.children[v2[i]]); 
-        p = *b;
-        cout<<p.key<<" "<<p.counter<<endl;
+    if(p->children.find(v2[i])!=p->children.end()){
+       p  = (p->children[v2[i]]); 
+        
+        cout<<p->key<<" "<<p->counter<<endl;
         
     }
     }
