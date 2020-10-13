e57ba72de5ff2ca0b2fb7bf6bf1676610e009fac
JaiJaveria <jaijaveria16@gmail.com>
2020-10-13 23:39:30 +0530

    trying fp growth

--- hw1/cond_fp.cpp
+++ hw1/cond_fp.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 
 // FP_tree cond_fp(FP_tree fp, FPNode* list,int val, int check){
-FP_tree cond_fp( FPNode* list, int check){
-    if(check==0){
+// FP_tree cond_fp( FPNode* list, int check){
+FP_tree cond_fp( FPNode* list){
+    // if(check==0){
         FPNode* temp= list;
         FP_tree cond;
-        while(temp!=NULL){
+        while(temp!=NULL)
+        {
             int count = temp->counter;
             stack<int> s;
             FPNode* parent = temp->parent;
@@ -26,27 +28,36 @@ FP_tree cond_fp( FPNode* list, int check){
             // v.clear();
             temp = temp->next;
         }
-    }
-    else
-    {
-     // to be completed for the case of conditional fp_tree inside a conditional fp_tree
-    }
+    // }
+    // else
+    // {
+    //  // to be completed for the case of conditional fp_tree inside a conditional fp_tree
+    // }
 }
-void mineFreq(FPTree* t, std::vector<int> *v, unordered_map<int,int> *freq, std::vector<pair<int,FPNode*>> *header/*just for order*/, unordered_map<int, int> *indexFind, float s )
+// void mineFreq(FPTree* t, std::vector<int> *vLeafs, unordered_map<int,int> *freq, std::vector<int> *prefix, std::vector<int> *ordered/*just for order*/, float s )
+void mineFreq(FPTree* t, FPNode *vLeafs, std::vector<pair<int,int>> *freq, std::vector<int> *prefix, std::vector<int> *ordered/*just for order*/, float s )
 {
     std::vector<int> p;
-    for( i:(*freq))
+    // FPTree cfd=*t;
+    FPNode *n;
+    p.assign(prefix->begin(), prefix->end());
+
+    for( int  i=freq->size()-1; i>=0;i--)
     {
         if(i.second>=s)
         {
-            p.clear();
-            p.assign(v->begin(), v->end());
-            p.push_back(i.first);
-            sort(p.begin(), p.end());
-            for ( n :p)
+            // printing the 
+            // p.clear();
+            p.push_back(i.first);//assuming the vector prefix is in decreasing order
+            // sort(p.begin(), p.end());
+            for ( int j=p.size(); j>=0; j--)
             {
-                cout << n << " ";
+                cout << p[j] << " ";
             }
+            //call mineFreq recursively.
+            n=cond_fp(vLeafs,validNodes);
+            //recursive call done.
+            p.pop_back();
             cout << "\n";
         }
     }
