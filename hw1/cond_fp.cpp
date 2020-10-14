fd945d89d0ffe4b958156104bb3accd85a250e17
JaiJaveria <jaijaveria16@gmail.com>
2020-10-14 14:34:21 +0530

    trying to cond fp

--- hw1/cond_fp.cpp
+++ hw1/cond_fp.cpp
@@ -6,8 +6,11 @@ using namespace std;
 
 // FP_tree cond_fp(FP_tree fp, FPNode* list,int val, int check){
 // FP_tree cond_fp( FPNode* list, int check){
-FP_tree cond_fp( FPNode* list){
+// FP_tree cond_fp( FPNode* list){
+FP_tree cond_fp( FPNode* list, unordered_set<int> *notValidNodes, std::vector<pair<int,int>> *freq ){
     // if(check==0){
+
+    //leafnodes list to be returned
         FPNode* temp= list;
         FP_tree cond;
         while(temp!=NULL)
@@ -15,8 +18,10 @@ FP_tree cond_fp( FPNode* list){
             int count = temp->counter;
             stack<int> s;
             FPNode* parent = temp->parent;
-            while(parent!=NULL){
-                s.push(parent.key);
+            while(parent!=NULL)
+            {
+                if(notValidNodes.find(parent.key)==notValidNodes.end())
+                    s.push(parent.key);
                 parent=parent->parent;
             }
             vector<int> v;
@@ -24,7 +29,7 @@ FP_tree cond_fp( FPNode* list){
                 v.push_back(s.top());
                 s.pop();
             }
-            insert(cond,v,count);
+            insert(cond,v,count, freq);
             // v.clear();
             temp = temp->next;
         }
@@ -35,30 +40,50 @@ FP_tree cond_fp( FPNode* list){
     // }
 }
 // void mineFreq(FPTree* t, std::vector<int> *vLeafs, unordered_map<int,int> *freq, std::vector<int> *prefix, std::vector<int> *ordered/*just for order*/, float s )
-void mineFreq(FPTree* t, FPNode *vLeafs, std::vector<pair<int,int>> *freq, std::vector<int> *prefix, std::vector<int> *ordered/*just for order*/, float s )
+
+void mineFreq( FPNode *vLeafs, std::unordered_map<int,int> *freq, std::vector<int> *order , std::vector<int> *prefix, float s )
 {
     std::vector<int> p;
     // FPTree cfd=*t;
-    FPNode *n;
+    unordered_set<int> notValidNodes;
+    FPNode *n=vLeafs;
     p.assign(prefix->begin(), prefix->end());
-
-    for( int  i=freq->size()-1; i>=0;i--)
+    //finding all nodes with less frequency
+    for( int  i=order->size()-1; i>=0;i--)
+    {
+        if(i.second<s)
+            notValidNodes.insert(i.first);
+    }
+    for( int  i=order->size()-1; i>=0;i--)
     {
         if(i.second>=s)
         {
-            // printing the 
-            // p.clear();
             p.push_back(i.first);//assuming the vector prefix is in decreasing order
-            // sort(p.begin(), p.end());
             for ( int j=p.size(); j>=0; j--)
             {
                 cout << p[j] << " ";
             }
             //call mineFreq recursively.
-            n=cond_fp(vLeafs,validNodes);
+            FPNode *newN;
+            // std::vector<pair<int,int>> newfreq, std::vector<int> newPre;
+            std::vector<pair<int,int>> newOrder;
+            newOrder.assign(order.begin(), order.find(i)-1);//debug
+            unordered_map<pair<int,int>> newFreq;
+            newN=cond_fp(n,&notValidNodes, &newfreq);
+            delete n;
+            n=newN;
+            mineFreq(n, &newF, &p, s);
+            // delete *n;
+            newfreq.clear();
+            // newPre.clear();
             //recursive call done.
             p.pop_back();
             cout << "\n";
         }
     }
+    // notValidNodes.clear();
+    // notValidNodes.shrink_to_fit();
+    delete n;
+    // p.clear();
+    // p.shrink_to_fit();
 }
