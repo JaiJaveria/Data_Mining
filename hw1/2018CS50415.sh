5fffc950c64cea5ce829dcfdc450880b4d0e0df1
pratik1011 <72463271+pratik1011@users.noreply.github.com>
2020-10-18 02:52:33 +0530

    Update 2018CS50415.sh

--- hw1/2018CS50415.sh
+++ hw1/2018CS50415.sh
@@ -3,10 +3,12 @@
 if [ "$3" == "-apriori" ]; then
         ./apriori $1 $2 $4
 elif [ "$3" == "-fptree" ]; then 
-        ./fptree $1 $2
+        ./fptree $1 $2 $4
 elif [ "$2" == "-plot" ]; then
+        module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
         python3 plot.py $1
         rm output
+        module unload pythonpackages/3.6.0/matplotlib/3.0.2/gnu
 else
     echo "Wrong command"
 fi
