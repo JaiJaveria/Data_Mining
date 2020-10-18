d5be1a05d4370f69ae4705edee4f4c518995d3e9
pratik1011 <72463271+pratik1011@users.noreply.github.com>
2020-10-18 20:59:31 +0530

    Update 2018CS50415.sh

--- 2018CS50415-Assgn1/2018CS50415.sh
+++ 2018CS50415-Assgn1/2018CS50415.sh
@@ -7,7 +7,7 @@ elif [ "$3" == "-fptree" ]; then
 elif [ "$2" == "-plot" ]; then
         module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
         python3 plot.py $1
-        rm output
+        rm plot_output
         module unload pythonpackages/3.6.0/matplotlib/3.0.2/gnu
 else
     echo "Wrong command"
